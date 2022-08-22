8f0b83d161508de852d81383206a65a91971bd89
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-08-22 03:59:25 +0300

    Done

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+def badge_maker name
+    return "Hello, my name is #{name}."
+  end
+
+  def batch_badge_creator array
+    array.map {|name| badge_maker(name)}
+  end
+
+  def assign_rooms array 
+    array.map.with_index {|name, idx| "Hello, #{name}! You'll be assigned to room #{idx + 1}!"}
+  end
+  def printer array 
+    batch_badge_creator(array).each {|item| puts item}
+    assign_rooms(array).each {|item| puts item}
+  end
+  
